1e2e970f85e352a5826dcbf715dc74f99e57989f
Van Via King Ali <vanviakingali@Vans-MacBook-Air.local>
2026-06-09 09:32:30 +0700

    fix: update register page logo to use new logo.svg

--- resources/views/livewire/forms/auth/register.blade.php
+++ resources/views/livewire/forms/auth/register.blade.php
@@ -2,7 +2,7 @@
   <div class="max-w-2xl mx-auto grid grid-cols-1 justify-center items-center h-screen">
     <div class="space-y-6">
       <div class="flex justify-center items-center">
-        <img src="{{ asset('assets/logo/image.png') }}" class="w-20 h-24" alt="Logo">
+        <img src="{{ asset('assets/logo/logo.svg') }}" class="w-20 h-24" alt="Logo">
       </div>
       <div class="w-full">
         <form>
