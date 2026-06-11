4819ce66bdc7f02bbaedb4d4898bc69a9171be89
Van Via King Ali <vanviakingali@Vans-MacBook-Air.local>
2026-06-12 01:16:30 +0700

    fix: activity_log subject_id string support + test DB config

--- database/migrations/2026_06_11_153448_create_activity_log_table.php
+++ database/migrations/2026_06_11_153448_create_activity_log_table.php
@@ -12,8 +12,12 @@ class CreateActivityLogTable extends Migration
             $table->bigIncrements('id');
             $table->string('log_name')->nullable();
             $table->text('description');
-            $table->nullableMorphs('subject', 'subject');
-            $table->nullableMorphs('causer', 'causer');
+            $table->string('subject_type')->nullable();
+            $table->string('subject_id')->nullable();
+            $table->index(['subject_id', 'subject_type'], 'subject');
+            $table->string('causer_type')->nullable();
+            $table->string('causer_id')->nullable();
+            $table->index(['causer_id', 'causer_type'], 'causer');
             $table->json('properties')->nullable();
             $table->timestamps();
             $table->index('log_name');
