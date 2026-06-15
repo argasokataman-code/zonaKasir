c58c1bd454f2faed166f62f5d407ccf3ef68cf31
Van Via King Ali <vanviakingali@Vans-MacBook-Air.local>
2026-06-15 11:52:03 +0700

    fix(tenant): remove table-fixed, use table-auto for report tables
    table-fixed forced equal-width columns causing text overlap.
    Switch to table-auto with text-sm + whitespace-nowrap on numeric
    cells so columns size to content naturally.

--- resources/views/reports/partials/purchasing-data.blade.php
+++ resources/views/reports/partials/purchasing-data.blade.php
@@ -7,42 +7,42 @@
   <div class="space-y-4">
     @foreach($reports as $key => $report)
       <div class="overflow-x-auto">
-        <x-table class="w-full table-fixed min-w-[700px]">
+        <x-table class="w-full text-sm">
           <x-table-header>
             <x-table-row>
-              <x-table-header-cell colspan="6"> {{ __('Supplier') }}:</x-table-header-cell>
-              <x-table-header-cell> {{ $report['supplier'] }}</x-table-header-cell>
+              <x-table-header-cell colspan="6">{{ __('Supplier') }}:</x-table-header-cell>
+              <x-table-header-cell>{{ $report['supplier'] }}</x-table-header-cell>
             </x-table-row>
             <x-table-row>
-              <x-table-header-cell colspan="6"> {{ __('Date') }}:</x-table-header-cell>
-              <x-table-header-cell> {{ $report['date'] }}</x-table-header-cell>
+              <x-table-header-cell colspan="6">{{ __('Date') }}:</x-table-header-cell>
+              <x-table-header-cell>{{ $report['date'] }}</x-table-header-cell>
             </x-table-row>
           </x-table-header>
           <x-table-header>
-            <x-table-header-cell>{{ __('Product name') }}</x-table-header-cell>
-            <x-table-header-cell>{{ __('Unit') }}</x-table-header-cell>
-            <x-table-header-cell>{{ __('Stock amount') }}</x-table-header-cell>
-            <x-table-header-cell>{{ __('Cost per stock') }}</x-table-header-cell>
-            <x-table-header-cell>{{ __('Total cost') }}</x-table-header-cell>
-            <x-table-header-cell>{{ __('Price per stock') }}</x-table-header-cell>
-            <x-table-header-cell>{{ __('Total price') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap">{{ __('Product name') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap">{{ __('Unit') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap text-right">{{ __('Stock amount') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap text-right">{{ __('Cost per stock') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap text-right">{{ __('Total cost') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap text-right">{{ __('Price per stock') }}</x-table-header-cell>
+            <x-table-header-cell class="whitespace-nowrap text-right">{{ __('Total price') }}</x-table-header-cell>
           </x-table-header>
           <tbody>
             @foreach ($report['stocks'] as $key => $stock)
               <x-table-row>
                 <x-table-cell>{{ $stock['product_name'] }}</x-table-cell>
-                <x-table-cell>{{ $stock['product_unit'] }}</x-table-cell>
-                <x-table-cell>{{ $stock['init_stock'] }}</x-table-cell>
-                <x-table-cell>{{ $stock['initial_price'] }}</x-table-cell>
-                <x-table-cell>{{ $stock['total_initial_price'] }}</x-table-cell>
-                <x-table-cell>{{ $stock['selling_price'] }}</x-table-cell>
-                <x-table-cell>{{ $stock['total_selling_price'] }}</x-table-cell>
+                <x-table-cell class="whitespace-nowrap">{{ $stock['product_unit'] }}</x-table-cell>
+                <x-table-cell class="number whitespace-nowrap">{{ $stock['init_stock'] }}</x-table-cell>
+                <x-table-cell class="number whitespace-nowrap">{{ $stock['initial_price'] }}</x-table-cell>
+                <x-table-cell class="number whitespace-nowrap">{{ $stock['total_initial_price'] }}</x-table-cell>
+                <x-table-cell class="number whitespace-nowrap">{{ $stock['selling_price'] }}</x-table-cell>
+                <x-table-cell class="number whitespace-nowrap">{{ $stock['total_selling_price'] }}</x-table-cell>
               </x-table-row>
             @endforeach
             <x-table-row class="border-t border-gray-200 dark:border-gray-600">
-              <x-table-cell colspan="4">{{ __('Subtotal') }}</x-table-cell>
-              <x-table-cell colspan="2">{{ $report['subtotal_total_initial_price'] }}</x-table-cell>
-              <x-table-cell>{{ $report['subtotal_total_selling_price'] }}</x-table-cell>
+              <x-table-cell colspan="4"><b>{{ __('Subtotal') }}</b></x-table-cell>
+              <x-table-cell colspan="2" class="number whitespace-nowrap"><b>{{ $report['subtotal_total_initial_price'] }}</b></x-table-cell>
+              <x-table-cell class="number whitespace-nowrap"><b>{{ $report['subtotal_total_selling_price'] }}</b></x-table-cell>
             </x-table-row>
           </tbody>
         </x-table>
@@ -51,18 +51,18 @@
   </div>
 
   <div class="overflow-x-auto mt-4">
-    <x-table class="w-full table-fixed min-w-[400px]">
+    <x-table class="w-full text-sm">
       <x-table-header>
         <x-table-row>
-          <x-table-header-cell colspan="2" style="text-align: center;">{{ __('Grand Total') }}</x-table-header-cell>
+          <x-table-header-cell colspan="2" class="text-center">{{ __('Grand Total') }}</x-table-header-cell>
         </x-table-row>
         <x-table-row>
-          <x-table-header-cell>{{ __('Cost') }}</x-table-header-cell>
-          <x-table-header-cell class="number" style="text-align: right;" class="text-right"><b>{{ $footer['grand_total_initial_price'] }}</b></x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Cost') }}</x-table-header-cell>
+          <x-table-header-cell class="number whitespace-nowrap text-right"><b>{{ $footer['grand_total_initial_price'] }}</b></x-table-header-cell>
         </x-table-row>
         <x-table-row>
-          <x-table-header-cell>{{ __('Selling price') }}</x-table-header-cell>
-          <x-table-header-cell class="number" style="text-align: right;" class="text-right"><b>{{ $footer['grand_total_selling_price'] }}</b></x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Selling price') }}</x-table-header-cell>
+          <x-table-header-cell class="number whitespace-nowrap text-right"><b>{{ $footer['grand_total_selling_price'] }}</b></x-table-header-cell>
         </x-table-row>
       </x-table-header>
     </x-table>
--- resources/views/reports/partials/selling-data.blade.php
+++ resources/views/reports/partials/selling-data.blade.php
@@ -6,73 +6,73 @@
   <p class="mb-4">{{ __('Period') }}: <b>{{ $header['start_date'] }} - {{ $header['end_date'] }}</b></p>
 
   <div class="overflow-x-auto">
-    <x-table class="w-full table-fixed min-w-[800px]">
+    <x-table class="w-full text-sm">
       <x-table-header>
-        <x-table-header-cell>@lang('SKU')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap">@lang('SKU')</x-table-header-cell>
         <x-table-header-cell>@lang('Product Name')</x-table-header-cell>
-        <x-table-header-cell>@lang('Price')</x-table-header-cell>
-        <x-table-header-cell>@lang('Qty')</x-table-header-cell>
-        <x-table-header-cell>@lang('Selling')</x-table-header-cell>
-        <x-table-header-cell>@lang('Discount')</x-table-header-cell>
-        <x-table-header-cell>@lang('Net Selling')</x-table-header-cell>
-        <x-table-header-cell>@lang('Gross Profit')</x-table-header-cell>
-        <x-table-header-cell>@lang('Net Profit')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Price')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Qty')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Selling')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Discount')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Net Selling')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Gross Profit')</x-table-header-cell>
+        <x-table-header-cell class="whitespace-nowrap text-right">@lang('Net Profit')</x-table-header-cell>
       </x-table-header>
 
       <tbody>
         @foreach($reports as $key => $report)
           <x-table-row>
-            <x-table-cell>{{ $report['sku'] }}</x-table-cell>
+            <x-table-cell class="whitespace-nowrap">{{ $report['sku'] }}</x-table-cell>
             <x-table-cell>{{ $report['name'] }}</x-table-cell>
-            <x-table-cell class="number">{{ $report['selling_price'] }}</x-table-cell>
-            <x-table-cell>{{ $report['qty'] }}</x-table-cell>
-            <x-table-cell class="number">{{ $report['selling'] }}</x-table-cell>
-            <x-table-cell class="number">{{ $report['discount_price'] }}</x-table-cell>
-            <x-table-cell class="number">{{ $report['total_after_discount'] }}</x-table-cell>
-            <x-table-cell class="number">{{ $report['gross_profit'] }}</x-table-cell>
-            <x-table-cell class="number">{{ $report['net_profit'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['selling_price'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['qty'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['selling'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['discount_price'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['total_after_discount'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['gross_profit'] }}</x-table-cell>
+            <x-table-cell class="number whitespace-nowrap">{{ $report['net_profit'] }}</x-table-cell>
           </x-table-row>
         @endforeach
         <x-table-row>
-          <x-table-cell colspan="3">{{ __('Total') }}</x-table-cell>
-          <x-table-cell class="number">{{ $footer['total_qty'] }}</x-table-cell>
-          <x-table-cell class="number">{{ $footer['total_gross'] }}</x-table-cell>
-          <x-table-cell class="number">{{ $footer['total_discount_per_item'] }}</x-table-cell>
-          <x-table-cell class="number">{{ $footer['total_net_price_after_discount_per_item'] }}</x-table-cell>
-          <x-table-cell class="number">{{ $footer['total_gross_profit'] }}</x-table-cell>
-          <x-table-cell class="number">{{ $footer['total_net_profit_before_discount_selling'] }}</x-table-cell>
+          <x-table-cell colspan="3"><b>{{ __('Total') }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap">{{ $footer['total_qty'] }}</x-table-cell>
+          <x-table-cell class="number whitespace-nowrap">{{ $footer['total_gross'] }}</x-table-cell>
+          <x-table-cell class="number whitespace-nowrap">{{ $footer['total_discount_per_item'] }}</x-table-cell>
+          <x-table-cell class="number whitespace-nowrap">{{ $footer['total_net_price_after_discount_per_item'] }}</x-table-cell>
+          <x-table-cell class="number whitespace-nowrap">{{ $footer['total_gross_profit'] }}</x-table-cell>
+          <x-table-cell class="number whitespace-nowrap">{{ $footer['total_net_profit_before_discount_selling'] }}</x-table-cell>
         </x-table-row>
       </tbody>
     </x-table>
   </div>
 
   <div class="overflow-x-auto mt-4">
-    <x-table class="w-full table-fixed min-w-[800px]">
+    <x-table class="w-full text-sm">
       <x-table-header>
         <x-table-row>
-          <x-table-header-cell colspan="8" class="text-center" style="text-align: center;">{{ __('Grand Total') }}</x-table-header-cell>
+          <x-table-header-cell colspan="8" class="text-center">{{ __('Grand Total') }}</x-table-header-cell>
         </x-table-row>
         <x-table-row>
-          <x-table-header-cell>{{ __('Cost') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Penjualan') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Discount per Penjualan') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Discount per Item') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Penjualan Setelah Discount') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Keuntungan Kotor') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Keuntungan Bersih Sebelum Diskon Penjualan') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Keuntungan Bersih Setelah Diskon Penjualan') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Cost') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Penjualan') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Discount per Penjualan') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Discount per Item') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Penjualan Setelah Discount') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Keuntungan Kotor') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Keuntungan Bersih Sebelum Diskon Penjualan') }}</x-table-header-cell>
+          <x-table-header-cell class="whitespace-nowrap">{{ __('Keuntungan Bersih Setelah Diskon Penjualan') }}</x-table-header-cell>
         </x-table-row>
       </x-table-header>
       <tbody>
         <x-table-row>
-          <x-table-cell class="number"><b>{{ $footer['total_cost'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_gross'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_discount'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_discount_per_item'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_net_price_after_discount_per_item'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_gross_profit'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_net_profit_before_discount_selling'] }}</b></x-table-cell>
-          <x-table-cell class="number"><b>{{ $footer['total_net_profit_after_discount_selling'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_cost'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_gross'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_discount'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_discount_per_item'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_net_price_after_discount_per_item'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_gross_profit'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_net_profit_before_discount_selling'] }}</b></x-table-cell>
+          <x-table-cell class="number whitespace-nowrap"><b>{{ $footer['total_net_profit_after_discount_selling'] }}</b></x-table-cell>
         </x-table-row>
       </tbody>
     </x-table>
