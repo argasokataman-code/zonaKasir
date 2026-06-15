3b9eaba921d3c2253a542a89c425dfde9145028d
Van Via King Ali <vanviakingali@Vans-MacBook-Air.local>
2026-06-15 11:46:02 +0700

    fix(tenant): add horizontal scroll to all report tables
    Wrap each table in overflow-x-auto div with min-w to prevent
    layout breakage on small screens and PWA.

--- resources/views/reports/partials/purchasing-data.blade.php
+++ resources/views/reports/partials/purchasing-data.blade.php
@@ -6,61 +6,65 @@
   <p class="mb-4">{{ __('Period') }}: <b>{{ $header['start_date'] }} - {{ $header['end_date'] }}</b></p>
   <div class="space-y-4">
     @foreach($reports as $key => $report)
-      <x-table class="w-full table-fixed">
-        <x-table-header>
-          <x-table-row>
-            <x-table-header-cell colspan="6"> {{ __('Supplier') }}:</x-table-header-cell>
-            <x-table-header-cell> {{ $report['supplier'] }}</x-table-header-cell>
-          </x-table-row>
-          <x-table-row>
-            <x-table-header-cell colspan="6"> {{ __('Date') }}:</x-table-header-cell>
-            <x-table-header-cell> {{ $report['date'] }}</x-table-header-cell>
-          </x-table-row>
-        </x-table-header>
-        <x-table-header>
-          <x-table-header-cell>{{ __('Product name') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Unit') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Stock amount') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Cost per stock') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Total cost') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Price per stock') }}</x-table-header-cell>
-          <x-table-header-cell>{{ __('Total price') }}</x-table-header-cell>
-        </x-table-header>
-        <tbody>
-          @foreach ($report['stocks'] as $key => $stock)
+      <div class="overflow-x-auto">
+        <x-table class="w-full table-fixed min-w-[700px]">
+          <x-table-header>
             <x-table-row>
-              <x-table-cell>{{ $stock['product_name'] }}</x-table-cell>
-              <x-table-cell>{{ $stock['product_unit'] }}</x-table-cell>
-              <x-table-cell>{{ $stock['init_stock'] }}</x-table-cell>
-              <x-table-cell>{{ $stock['initial_price'] }}</x-table-cell>
-              <x-table-cell>{{ $stock['total_initial_price'] }}</x-table-cell>
-              <x-table-cell>{{ $stock['selling_price'] }}</x-table-cell>
-              <x-table-cell>{{ $stock['total_selling_price'] }}</x-table-cell>
+              <x-table-header-cell colspan="6"> {{ __('Supplier') }}:</x-table-header-cell>
+              <x-table-header-cell> {{ $report['supplier'] }}</x-table-header-cell>
             </x-table-row>
-          @endforeach
-          <x-table-row class="border-t border-gray-200 dark:border-gray-600">
-            <x-table-cell colspan="4">{{ __('Subtotal') }}</x-table-cell>
-            <x-table-cell colspan="2">{{ $report['subtotal_total_initial_price'] }}</x-table-cell>
-            <x-table-cell>{{ $report['subtotal_total_selling_price'] }}</x-table-cell>
-          </x-table-row>
-        </tbody>
-      </x-table>
-      @endforeach
+            <x-table-row>
+              <x-table-header-cell colspan="6"> {{ __('Date') }}:</x-table-header-cell>
+              <x-table-header-cell> {{ $report['date'] }}</x-table-header-cell>
+            </x-table-row>
+          </x-table-header>
+          <x-table-header>
+            <x-table-header-cell>{{ __('Product name') }}</x-table-header-cell>
+            <x-table-header-cell>{{ __('Unit') }}</x-table-header-cell>
+            <x-table-header-cell>{{ __('Stock amount') }}</x-table-header-cell>
+            <x-table-header-cell>{{ __('Cost per stock') }}</x-table-header-cell>
+            <x-table-header-cell>{{ __('Total cost') }}</x-table-header-cell>
+            <x-table-header-cell>{{ __('Price per stock') }}</x-table-header-cell>
+            <x-table-header-cell>{{ __('Total price') }}</x-table-header-cell>
+          </x-table-header>
+          <tbody>
+            @foreach ($report['stocks'] as $key => $stock)
+              <x-table-row>
+                <x-table-cell>{{ $stock['product_name'] }}</x-table-cell>
+                <x-table-cell>{{ $stock['product_unit'] }}</x-table-cell>
+                <x-table-cell>{{ $stock['init_stock'] }}</x-table-cell>
+                <x-table-cell>{{ $stock['initial_price'] }}</x-table-cell>
+                <x-table-cell>{{ $stock['total_initial_price'] }}</x-table-cell>
+                <x-table-cell>{{ $stock['selling_price'] }}</x-table-cell>
+                <x-table-cell>{{ $stock['total_selling_price'] }}</x-table-cell>
+              </x-table-row>
+            @endforeach
+            <x-table-row class="border-t border-gray-200 dark:border-gray-600">
+              <x-table-cell colspan="4">{{ __('Subtotal') }}</x-table-cell>
+              <x-table-cell colspan="2">{{ $report['subtotal_total_initial_price'] }}</x-table-cell>
+              <x-table-cell>{{ $report['subtotal_total_selling_price'] }}</x-table-cell>
+            </x-table-row>
+          </tbody>
+        </x-table>
+      </div>
+    @endforeach
   </div>
-  <x-table class="w-full table-fixed mt-4">
-    <x-table-header>
-      <x-table-row>
-        <x-table-header-cell colspan="2" style="text-align: center;">{{ __('Grand Total') }}</x-table-header-cell>
-      </x-table-row>
-      <x-table-row>
-        <x-table-header-cell>{{ __('Cost') }}</x-table-header-cell>
-        <x-table-header-cell class="number" style="text-align: right;" class="text-right"><b>{{ $footer['grand_total_initial_price'] }}</b></x-table-header-cell>
-      </x-table-row>
-      <x-table-row>
-        <x-table-header-cell>{{ __('Selling price') }}</x-table-header-cell>
-        <x-table-header-cell class="number" style="text-align: right;" class="text-right"><b>{{ $footer['grand_total_selling_price'] }}</b></x-table-header-cell>
-      </x-table-row>
-    </x-table-header>
-  </x-table>
-</div>
 
+  <div class="overflow-x-auto mt-4">
+    <x-table class="w-full table-fixed min-w-[400px]">
+      <x-table-header>
+        <x-table-row>
+          <x-table-header-cell colspan="2" style="text-align: center;">{{ __('Grand Total') }}</x-table-header-cell>
+        </x-table-row>
+        <x-table-row>
+          <x-table-header-cell>{{ __('Cost') }}</x-table-header-cell>
+          <x-table-header-cell class="number" style="text-align: right;" class="text-right"><b>{{ $footer['grand_total_initial_price'] }}</b></x-table-header-cell>
+        </x-table-row>
+        <x-table-row>
+          <x-table-header-cell>{{ __('Selling price') }}</x-table-header-cell>
+          <x-table-header-cell class="number" style="text-align: right;" class="text-right"><b>{{ $footer['grand_total_selling_price'] }}</b></x-table-header-cell>
+        </x-table-row>
+      </x-table-header>
+    </x-table>
+  </div>
+</div>
