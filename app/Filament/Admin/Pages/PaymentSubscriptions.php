e6aa59e7265e23e822eb0580fa2e44418714aff6
Van Via King Ali <vanviakingali@Vans-MacBook-Air.local>
2026-06-15 10:02:41 +0700

    fix(admin): Tenant model namespace App\Tenant not App\Models\Tenant

--- app/Filament/Admin/Pages/PaymentSubscriptions.php
+++ app/Filament/Admin/Pages/PaymentSubscriptions.php
@@ -4,7 +4,7 @@ namespace App\Filament\Admin\Pages;
 
 use App\Models\Invoice;
 use App\Models\Subscription;
-use App\Models\Tenant;
+use App\Tenant;
 use Filament\Pages\Page;
 use Illuminate\Support\Facades\DB;
 
