0a9bcdf6788e970a8510b2e7d42cf06d37de3993
Van Via King Ali <vanviakingali@Vans-MacBook-Air.local>
2026-06-19 10:57:29 +0700

    chore: track build artifacts for staging deploy

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '502ab4ef'; // bumped: fix PWA offline blank modal popup
+const CACHE_VERSION = 'f57538ab'; // bumped: fix PWA offline blank modal popup
 const STATIC_CACHE = `zonakasir-static-${CACHE_VERSION}`;
 const PAGES_CACHE = `zonakasir-pages-${CACHE_VERSION}`;
 const API_CACHE = `zonakasir-api-${CACHE_VERSION}`;
