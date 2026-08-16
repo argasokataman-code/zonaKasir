caeaa28a807ab877b1608101afdcb4fe0d009305
Arga Sokatawan <argasokataman@gmail.com>
2026-08-16 18:58:24 +0700

    fix(auth): register Socialite provider (survived branch reset)
    packages.php regenerated with Socialite + manual provider in config/app.php.
    Reset to aa26b332 had dropped these, breaking Socialite resolution on Vercel.

--- config/app.php
+++ config/app.php
@@ -191,6 +191,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Application Service Providers...
